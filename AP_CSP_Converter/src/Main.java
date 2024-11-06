c0ed521bac2060e5a18dae0c9220d56ea2d50efa
楠z✰HisakoR <3389075400@qq.com>
2024-11-06 22:57:51 +0800

    同步本地

--- AP_CSP_Converter/src/Main.java
+++ AP_CSP_Converter/src/Main.java
@@ -1,5 +1,19 @@
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("gui/main.fxml"));
+        primaryStage.setTitle("大混战KC ~ v0.01");
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+    }
+    //主要方法
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(args);
     }
 }
