49840700bdcc70d4f97c8e9497cc4ae8aee6e8ee
楠z✰HisakoR <3389075400@qq.com>
2024-11-11 22:11:17 +0800

    添加了路径拾取位置
    移除了对output文件夹的同步

--- AP_CSP_Converter/src/gui/controller.java
+++ AP_CSP_Converter/src/gui/controller.java
@@ -1,6 +1,7 @@
 package gui;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
 
@@ -11,11 +12,13 @@ public class controller {
     private final filer filer = new filer();
     @FXML
     private Text texter;
+    @FXML
+    private TextField filePath;
 
     @FXML
     public void getFileInfo(MouseEvent event) throws IOException {
-        texter.setText(filer.readFile("src/lol.txt").get(2));
-        filer.saveData(filer.readFile("src/lol.txt"));
+        texter.setText(filer.readFile(filePath.getText()).get(0));
+        filer.saveData(filer.readFile(filePath.getText()));
 
     }
 }
